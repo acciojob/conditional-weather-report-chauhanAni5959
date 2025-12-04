5af78c9e90c3de22c6949f5b67e0c3dac00266f5
anisingh <anisingh565072@gmail.com>
2025-12-04 07:54:53 +0530

    Assignment Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
+import React, { useState, useEffect } from "react";
+import WeatherDisplay from "./WeatherDisplay";
 
-import React from "react";
-import './../styles/App.css';
+function App() {
+  const [weather, setWeather] = useState({
+    temperature: 25,
+    conditions: "Sunny",
+  });
 
-const App = () => {
+  
+  useEffect(() => {
+    
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <WeatherDisplay weather={weather} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
